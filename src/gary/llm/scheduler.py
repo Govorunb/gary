6964a55e3a4ee63f1596eaf982cc06ab4e0e9e8d
Govorunb <Govorunb@users.noreply.github.com>
2025-02-10 13:36:03 +1100

    refactor scheduler slightly

--- src/gary/llm/scheduler.py
+++ src/gary/llm/scheduler.py
@@ -1,94 +1,146 @@
 import asyncio
 from typing import TYPE_CHECKING, Any, Callable, Coroutine
-from ..util import CONFIG, logger
 
+from ..util import CONFIG, logger
 if TYPE_CHECKING:
     from ..registry import Game
 
 _COMPLETED_TASK = asyncio.Task(asyncio.sleep(0))
+_DEBUG = False
+_logger = logger.getChild(__name__)
+
+class PeriodicTimer:
+    def __init__(self, interval: float, callback: Callable[..., Coroutine[Any, Any, bool]], name: str = "timer", start_immediately: bool = False):
+        assert interval >= 0, "Interval must be at least 0"
+        self._interval = interval
+        self.callback = callback
+        self.name = name
+        self._task: asyncio.Task[None] = _COMPLETED_TASK
+        self._active = False
+        self.debug = _DEBUG
+        if start_immediately:
+            self.start()
+
+    def start(self) -> None:
+        if self._active:
+            return
+        self._debug(f"Starting {self.name}")
+        self._active = True
+        self.reset()
+
+    def stop(self) -> None:
+        if not self._active:
+            return
+        self._debug(f"Stopping {self.name}")
+        self._active = False
+        self._task.cancel()
+
+    def reset(self) -> None:
+        self._debug(f"Resetting {self.name}")
+        self._task.cancel()
+        if not self._active:
+            return
+        self._task = asyncio.create_task(self._run()) if self._interval > 0 else _COMPLETED_TASK
+
+    @property
+    def active(self) -> bool:
+        return self._active
+
+    @property
+    def interval(self) -> float:
+        return self._interval
+
+    @interval.setter
+    def interval(self, new_interval: float) -> None:
+        self._interval = new_interval
+        self.reset()
+
+    async def _run(self):
+        self._debug(f"{self.name} sleeping for {self._interval}")
+        await asyncio.sleep(self._interval)
+        self._debug(f"{self.name} woke up: {'active' if self._active else 'not active'}")
+        if not self._active:
+            return
+        restart = await self.callback()
+        self._debug(f"{self.name} executed callback: {'continue' if restart else 'done'}")
+        if restart:
+            self.reset()
+    
+    def _debug(self, msg: str) -> None:
+        if not self.debug:
+            return
+        _logger.debug(msg)
 
 class Scheduler:
     def __init__(self, game: "Game"):
         logger.info(f"Created scheduler for {game.name}")
         self.game = game
-        # FIXME: actual scheduling (queue and such)
-        self.idle_timeout_try = CONFIG.gary.scheduler.idle_timeout_try
-        self.idle_timeout_force = CONFIG.gary.scheduler.idle_timeout_force
+        idle_timeout_try = CONFIG.gary.scheduler.idle_timeout_try
+        idle_timeout_force = CONFIG.gary.scheduler.idle_timeout_force
 
-        if not self.idle_timeout_try:
+        if not idle_timeout_try:
             logger.info("Idle timeout (try) disabled")
-        if not self.idle_timeout_force:
+        if not idle_timeout_force:
             logger.info("Idle timeout (force) disabled")
 
         self._active = False
-        self._try_task = _COMPLETED_TASK
-        self._force_task = _COMPLETED_TASK
+        self._try_timer = PeriodicTimer(
+            idle_timeout_try,
+            self._try,
+            name="try_timer",
+        )
+        self._force_timer = PeriodicTimer(
+            idle_timeout_force,
+            self._force,
+            name="force_timer",
+        )
 
     def start(self):
         if self._active:
             return
         self._active = True
-        self._reset_idle_timers()
+        self._try_timer.start()
+        self._force_timer.start()
 
     def stop(self):
         if not self._active:
             return
         self._active = False
-        self._try_task.cancel()
-        self._force_task.cancel()
+        self._try_timer.stop()
+        self._force_timer.stop()
 
     def on_action(self, *_):
-        self._reset_idle_timers()
+        if not self._active:
+            return
+        self._try_timer.reset()
+        self._force_timer.reset()
 
     def on_context(self, *_):
-        self._reset_try()
-
-    def _reset_idle_timers(self):
         if not self._active:
             return
-        self._reset_try()
-        self._reset_force()
+        self._try_timer.reset()
 
-    def _reset_try(self):
-        self._try_task = self._reset_task(self._try_task, self._wait_try) \
-            if self.idle_timeout_try > 0 \
-            else _COMPLETED_TASK
-    def _reset_force(self):
-        self._force_task = self._reset_task(self._force_task, self._wait_force) \
-            if self.idle_timeout_force > 0 \
-            else _COMPLETED_TASK
-
-    def _reset_task(self, task: asyncio.Task[None], fn: Callable[..., Coroutine[Any, Any, None]]) -> asyncio.Task[None]:
-        task.cancel()
-        if not self._active:
-            return _COMPLETED_TASK
-        return asyncio.create_task(fn())
-
-    async def _wait_try(self):
-        delay = self.idle_timeout_try
-        await asyncio.sleep(delay)
+    async def _try(self):
         if not self.game.connection.is_connected():
             self.stop()
-            return
+            return False
         if not self.game.actions and not CONFIG.gary.allow_yapping:
-            logger.info(f"Idled for {delay}s, but no actions")
-            self._reset_try()
-            return
-        logger.warning(f"Idled for {delay}s, trying action")
-        if not await self.game.try_action(): # if True, will reset
-            self._reset_try()
+            logger.info(f"Idled for {self._try_timer.interval}s, but no actions")
+            return True
+        logger.warning(f"Idled for {self._try_timer.interval}s, trying action")
+        return not await self.game.try_action() # if acted, on_action will reset
 
-    async def _wait_force(self):
-        delay = self.idle_timeout_force
-        await asyncio.sleep(delay)
+    async def _force(self):
         if not self.game.connection.is_connected():
             self.stop()
-            return
+            return False
         if not self.game.actions:
-            logger.info(f"Didn't do anything for {delay}s! But nothing to force")
-            self._reset_idle_timers()
-            return
-        self._try_task.cancel()
-        logger.error(f"Didn't do anything after {delay}s! Forcing")
-        if not await self.game._force_any_action():
-            self._reset_idle_timers()
+            logger.info(f"Didn't do anything for {self._force_timer.interval}s! But nothing to force")
+            return True
+        # FIXME: timer awkwardness where they can overlap/both try to execute at the same time (while LLM is blocking)
+        # queue scheduler will come eventually
+        logger.error(f"Didn't do anything after {self._force_timer.interval}s! Forcing")
+        retry = not await self.game._force_any_action()
+        if retry:
+            self._try_timer.reset()
+        return retry
