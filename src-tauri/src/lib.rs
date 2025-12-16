b920ad25a3eac5fe55a244e3fe5671bf62b18200
Govorunb <Govorunb@users.noreply.github.com>
2025-12-16 00:03:56 +0000

    not using native dialogs anymore

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -34,7 +34,6 @@ pub fn run() {
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![
             is_server_running, server_state, start_server, stop_server,
             ws_accept, ws_deny, ws_send, ws_close,
