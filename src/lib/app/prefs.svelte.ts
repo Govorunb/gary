2f5e79dc39d79bbffb1004f940c37c94703ed5e1
Govorunb <Govorunb@users.noreply.github.com>
2026-01-02 21:43:38 +0000

    bump version
    hopefully this one is it

--- src/lib/app/prefs.svelte.ts
+++ src/lib/app/prefs.svelte.ts
@@ -165,7 +165,7 @@ export type ApiPrefs = z.infer<typeof zApiPrefs>;
 
 const MIGRATIONS: Migration[] = [
     {
-        version: "1.0.0-beta.0",
+        version: "1.0.0-beta.2",
         description: "created .api and moved .server into it (.server.port -> .api.server.port)",
         migrate(data) {
             if (!data) return;
