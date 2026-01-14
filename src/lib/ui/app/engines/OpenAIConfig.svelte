b2608d934a9699ccc7f803007b99d8e8e9c32435
Govorunb <Govorunb@users.noreply.github.com>
2026-01-14 13:57:32 +0000

    warn against switching to `/api` for ollama

--- src/lib/ui/app/engines/OpenAIConfig.svelte
+++ src/lib/ui/app/engines/OpenAIConfig.svelte
@@ -2,6 +2,7 @@
     import { dev } from '$app/environment';
     import { zOpenAIPrefs } from '$lib/app/engines/llm/openai.svelte';
     import { StringField, BooleanField, UrlField } from '$lib/ui/common/form';
+    import Hotkey from '$lib/ui/common/Hotkey.svelte';
     import OutLink from '$lib/ui/common/OutLink.svelte';
     import EngineConfig from './EngineConfig.svelte';
 
@@ -11,12 +12,25 @@
 
 <EngineConfig {engineId} {schema} {close}>
     {#snippet configForm(dirtyConfig)}
+        {@const isOllama = engineId === 'ollama'
+            || dirtyConfig.name.match(/\bollama\b/i)
+            || dirtyConfig.serverUrl.match(":11434/")}
+        {@const isWindows = dev || navigator.platform.includes("Win32")}
         <StringField
             bind:value={dirtyConfig.name}
             label="Name"
             required
             description="Display name for this engine"
         />
+        {#if isOllama && isWindows}
+            <div class="callout warn">
+                <p class="note">
+                    Ollama on Windows requires a user workaround.
+                    See <OutLink href="https://github.com/Govorunb/gary/issues/7">issue #7</OutLink>.
+                    {#if dev}(shown on any OS in dev mode){/if}
+                </p>
+            </div>
+        {/if}
         <StringField
             bind:value={dirtyConfig.apiKey}
             label="API Key"
@@ -29,12 +43,11 @@
             placeholder="https://api.openai.com/v1"
             description="Base URL for the API"
         />
-        {#if dirtyConfig.serverUrl.includes(":11434/v1") && (dev || navigator.platform.includes("Win32"))}
-            <div class="callout warn">
-                <p class="note">
-                    Ollama on Windows requires a user workaround.
-                    See <OutLink href="https://github.com/Govorunb/gary/issues/7">issue #7</OutLink>.
-                    {#if dev}(shown on all OSes in dev mode){/if}
+        {#if isOllama && dirtyConfig.serverUrl.match(/https?:\/\/[^/]+\/api/i)}
+            <div class="callout">
+                <p class="note whitespace-pre-line">
+                    Gary does not support Ollama's <OutLink href="https://docs.ollama.com/api/introduction">custom API</OutLink> on <Hotkey>/api</Hotkey>.
+                    Please use the <OutLink href="https://docs.ollama.com/api/openai-compatibility">OpenAI-compatible API</OutLink> on <Hotkey>/v1</Hotkey>.
                 </p>
             </div>
         {/if}
